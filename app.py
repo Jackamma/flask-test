218f9e68aaf9627f525347ca8222136a7b018fd6
Daniele <8095437+Jackamma@users.noreply.github.com>
2021-03-22 01:38:45 +0100

    First working release

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import peewee
+from time import time
+
+db = peewee.SqliteDatabase('tasks.db')
+
+class task(peewee.Model):
+	text = peewee.TextField()
+	date = peewee.TimestampField()
+
+	class Meta:
+
+		database = db
+		db_table = 'tasks'
+
+task.create_table()
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        passtask_content = request.form['content']
+        print(passtask_content)
+        new = task.create(text=passtask_content, date=time())
+        new.save()
+        tasks = task.select()
+        return render_template('index.html', tasks=tasks)
+    else:
+        tasks = task.select()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    res = task.delete().where(task.id==id).execute()
+
+    print(res)
+
+    if res > 0:
+        return redirect('/')
+    else:
+        return 'Errore! Non sono riuscito a eliminare la riga.'
 
 if __name__ == '__main__':
     app.run(debug=True)
